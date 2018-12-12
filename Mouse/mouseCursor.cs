7d672edcf0148ccb60681a77530f434b3babc01a
Ziyuan Wang <wangzy1996@yahoo.com>
2018-12-12 03:19:25 -0500

    Add files via upload

---
+++ Mouse/mouseCursor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseCursor : MonoBehaviour {
+
+    private SpriteRenderer rend;
+    public Sprite clickCursor;
+    public Sprite normalCursor;
+
+	void Start () {
+        Cursor.visible = false;
+        rend = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+        if (Input.GetMouseButtonDown(0))
+        {
+            rend.sprite = clickCursor;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            rend.sprite = normalCursor;
+        }
+
+    }
+}
